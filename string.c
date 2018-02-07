d4f0140aaa9a0266ddb266ae98502acf6010e382
sivaranjini1698 <31438762+sivaranjini1698@users.noreply.github.com>
2018-02-07 16:44:48 +0530

    string.c
    program

---
+++ string.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+voidmain()
+{
+int num,count=0;
+printf)enter number");
+scanf("%d"&num);
+while(num)
+num=num
+count+
+}
+printf("no of string digits %d",&count
+}
